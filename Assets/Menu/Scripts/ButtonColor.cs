28d814c078ffb8badcf6e12084aa4cc54723bc3a
David Whitters <whitterd@mail.gvsu.edu>
2017-11-03 23:41:48 -0400

    Added a basic main menu

---
+++ Assets/Menu/Scripts/ButtonColor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class ButtonColor : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler {
+
+	// Used for the highlighted start button state text.
+	private Color darkGreen = new Color(0, 0.5F, 0);
+	// Used for the highlighted quit button state text.
+	private Color darkRed = new Color(0.5F, 0, 0);
+
+	// This button object's text child.
+	private Text myText;
+
+	// Called on startup
+	public void Start() {
+		myText = gameObject.GetComponentInChildren<Text> ();
+	}
+	// Set the text color to green on hover.
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		if (myText.text.Equals ("Start")) {
+			myText.color = darkGreen;
+		} else {
+			myText.color = darkRed;
+		}
+	}
+	// Set the text color to black on hover exit.
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		myText.color = Color.black;
+	}
+
+	// Set the text color to light green on select.
+	public void OnSelect(BaseEventData eventData)
+	{
+		if (myText.text.Equals ("Start")) {
+			myText.color = Color.green;
+		} else {
+			myText.color = Color.red;
+		}
+	}
+}
---
+++ Assets/Menu/Scripts/MenuActions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MenuActions : MonoBehaviour, ISelectHandler {
+
+	// This button object's text child.
+	private Text myText;
+
+	// Called on startup
+	public void Start() {
+		myText = gameObject.GetComponentInChildren<Text> ();
+	}
+
+	// Set the text color to light green on select.
+	public void OnSelect(BaseEventData eventData)
+	{
+		if (myText.text.Equals ("Start")) {
+			SceneManager.LoadScene ("Scene1");
+		} else {
+			Application.Quit ();
+		}
+	}
+}
