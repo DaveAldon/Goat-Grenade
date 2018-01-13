6fe35aeae525774053a601c15cf3084940b53ce5
DaveAldon <crawford@mail.gvsu.edu>
2018-01-13 01:54:40 -0500

    Poop cleans up after some time.

---
+++ Assets/Level Assets/Scripts/PooBehavior.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PooBehavior : MonoBehaviour {
+
+	void Start () {
+        StartCoroutine(Clean());
+	}
+	
+    public IEnumerator Clean()
+    {
+        yield return new WaitForSeconds(20);
+        Destroy(gameObject);
+    }
+}
