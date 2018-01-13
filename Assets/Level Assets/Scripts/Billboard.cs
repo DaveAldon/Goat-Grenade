8e21f82310d45b732ad624b35b76e0b9771acf31
DaveAldon <crawford@mail.gvsu.edu>
2018-01-13 02:30:20 -0500

    Added healthbars to sheep. The billboard script makes them face the camera's rotation like a sprite. The provided Unity billboard script didn't accomplish this correctly so it's slightly different.

---
+++ Assets/Level Assets/Scripts/Billboard.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+
+    void Update () {
+        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
+    }
+}
---
+++ Assets/Level Assets/Scripts/SheepHealthBar.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SheepHealthBar : MonoBehaviour
+{
+    public int health = 0;
+    public RectTransform healthBar;
+
+    public void LateUpdate()
+    {
+        healthBar.sizeDelta = new Vector2(GetComponent<BaseSheepAttribute>().health, healthBar.sizeDelta.y);
+    }
+}
